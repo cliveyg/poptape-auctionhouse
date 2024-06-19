0785607434d819e189dd836e246bb1c2c5afe204
cliveyg <junkcat@gmail.com>
2024-06-19 14:29:09 -0300

    Adding api tests

--- auctionhouse/tests/test_api.py
+++ auctionhouse/tests/test_api.py
@@ -15,7 +15,6 @@ from rest_framework.test import APIClient
 class TestAPIPaths(TestCase):
 
     def test_status_ok_no_auth(self):
-    #    c = APIClient()
-    #    r = c.get('auctionhouse/status')
-    #    assert r.status_code == 200
-        assert 1 == 1
+        c = APIClient()
+        r = c.get('auctionhouse/status')
+        assert r.status_code == 200

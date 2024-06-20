7f5eaee6bec55b7ba14d5fa504914c5ea075830f
cliveyg <junkcat@gmail.com>
2024-06-20 15:18:06 -0300

    Adding api tests

--- auctionhouse/tests/test_setup.py
+++ auctionhouse/tests/test_setup.py
@@ -4,26 +4,7 @@ from auction.models import Auction
 from auction.models import Testy
 import uuid
 from datetime import datetime, timedelta
-#from django_unixdatetimefield import UnixDateTimeField
-
-
-def create_test(cls):
-
-    test_id = str(uuid.uuid4())
-    now = datetime.now()
-    tomorrow = now + timedelta(days=1)
-
-    cls.Test1 = Testy.objects.create(
-        test_id = test_id,
-        public_id = str(uuid.uuid4()),
-        type = "English",
-        name = "Testttty",
-        multiple = False,
-        start_time = now,
-        end_time = tomorrow,
-        status = "created"
-    )
-    return test_id
+# from django_unixdatetimefield import UnixDateTimeField
 
 
 def create_auction(cls):
@@ -46,21 +27,3 @@ def create_auction(cls):
         currency = "GBP"
     )
     return auction_id
-#auction_id = models.CharField(max_length=36, blank=False, primary_key=True,
-#                              validators=[validate_uuid_from_model])
-#public_id = models.CharField(max_length=36, blank=False,
-#                             validators=[validate_uuid_from_model])
-#lots = ArrayField(models.CharField(max_length=36,
-#                                   blank=False, null=False,
-#                                   validators=[validate_uuid_from_model]), size=500)
-#type = models.CharField(max_length=15,
-#                        choices=AuctionType.AUCTION_CHOICES)
-#name = models.CharField(max_length=100, blank=True)
-#multiple = models.BooleanField(default=False, null=False)
-#start_time = UnixDateTimeField(blank=True)
-#end_time = UnixDateTimeField(blank=True)
-#status = models.CharField(max_length=20, blank=False, default="created")
-#active = models.BooleanField(default=False)
-#created = models.DateTimeField(auto_now_add=True)
-#modified = models.DateTimeField(auto_now=True)
-#currency = models.CharField(max_length=3, blank=False)

457bdee440ab51dd2c70f756ba4bdcce6f50aed9
cliveyg <junkcat@gmail.com>
2024-06-21 20:09:43 -0300

    Add more api tests

--- auction/tests/test_models.py
+++ auction/tests/test_models.py
@@ -1,5 +1,40 @@
 from django.test import TestCase
+from auction.models import EnglishAuctionLot, AuctionLot
+import uuid
+import pytz
+from datetime import datetime, timedelta
+from django.conf import settings
+
+NOW = datetime.utcnow()
+NOW_PLUS_TEN = NOW + timedelta(days=10)
+timey = NOW
+NOW_WITH_TZ = timey.replace(tzinfo=pytz.timezone(settings.TIME_ZONE))
+LOT_ID = str(uuid.uuid4())
+ITEM_ID = str(uuid.uuid4())
+
 
 class ModelsTestCase(TestCase):
 
-    pass
+    def setUp(self):
+        print('test_setUp')
+        """
+        self.english_auc = EnglishAuctionLot.objects.create(lot_id = LOT_ID,
+                                                            item_id = ITEM_ID,
+                                                            start_time = NOW,
+                                                            end_time = NOW_PLUS_TEN,
+                                                            status = 'created',
+                                                            active = True,
+                                                            created = NOW_WITH_TZ,
+                                                            modified = NOW_WITH_TZ,
+                                                            quantity = 1,
+                                                            start_price = 99.50,
+                                                            reserve_price = 145.80,
+                                                            min_increment = 0.50)
+                                                            """
+        self.english_auc = EnglishAuctionLot.objects.create(start_price = 99.50,
+                                                            reserve_price = 145.80,
+                                                            min_increment = 0.50)
+
+    def test_get_english_auction(self):
+        # eng_auc_test = EnglishAuctionLot.objects.get(lot_id = LOT_ID)
+        self.assertEqual(self.english_auc.start_price, 99.50)
--- auctionhouse/tests/test_api.py
+++ auctionhouse/tests/test_api.py
@@ -1,4 +1,6 @@
 # auctionhouse/tests/test_api.py
+import uuid
+
 from django.test import TransactionTestCase
 from rest_framework.test import RequestsClient
 from .test_setup import create_auction_and_lots
@@ -11,13 +13,18 @@ logger = logging.getLogger(__name__)
 
 
 def mocked_auth_success(*args, **kwargs):
-
     r = Mock(spec=Response)
     r.status_code = 200
     r.headers = {'Content-Type': 'application/json'}
     r.json.return_value = {'public_id': 'Yarp'}
     return r
 
+def mocked_auth_fail_403(*args, **kwargs):
+    r = Mock(spec=Response)
+    r.status_code = 200
+    r.headers = {'Content-Type': 'application/json'}
+    r.json.return_value = {'public_id': 'Yarp'}
+    return r
 
 # helper function to compare json objects
 def ordered(obj):
@@ -43,6 +50,29 @@ class TestAPIPaths(TransactionTestCase):
         header = {'x-access-token': 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNfaWQiOiJmMzhiYTM5YS0zNjgyLTQ4MDMtYTQ5OC02NTlmMGJmMDUzMDQiLCJ1c2VybmFtZSI6ImNsaXZleSIsImV4cCI6MTcxOTAxNDMxNX0.-qkVpCAZvwng-Suf55EPLAd4r-PHgVqqYFywjDtjnrUNL8hsdYyFMgFFPdE1wOhYYjI9izftfyY43pUayEQ57g'}
         r = c.get('http://localhost/auctionhouse/auction/'+self.auction_id, headers=header)
         assert r.status_code == 200
+        assert r.headers.get('Content-Type') == 'application/json'
+
+    def test_fail_get_auction_no_auth(self, mock_get):
+        c = RequestsClient()
+        r = c.get('http://localhost/auctionhouse/auction/'+self.auction_id)
+        assert r.status_code == 403
+        assert r.headers.get('Content-Type') == 'application/json'
+
+    @mock.patch('auctionhouse.authentication.requests.get', side_effect=mocked_auth_success)
+    def test_fail_get_auction_by_id_404(self, mock_get):
+        c = RequestsClient()
+        header = {'x-access-token': 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNfaWQiOiJmMzhiYTM5YS0zNjgyLTQ4MDMtYTQ5OC02NTlmMGJmMDUzMDQiLCJ1c2VybmFtZSI6ImNsaXZleSIsImV4cCI6MTcxOTAxNDMxNX0.-qkVpCAZvwng-Suf55EPLAd4r-PHgVqqYFywjDtjnrUNL8hsdYyFMgFFPdE1wOhYYjI9izftfyY43pUayEQ57g'}
+        r = c.get('http://localhost/auctionhouse/auction/'+str(uuid.uuid4()), headers=header)
+        assert r.status_code == 404
+        assert r.headers.get('Content-Type') == 'application/json'
+
+    @mock.patch('auctionhouse.authentication.requests.get', side_effect=mocked_auth_success)
+    def test_fail_get_auction_by_id_not_valid_uuid(self, mock_get):
+        c = RequestsClient()
+        header = {'x-access-token': 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNfaWQiOiJmMzhiYTM5YS0zNjgyLTQ4MDMtYTQ5OC02NTlmMGJmMDUzMDQiLCJ1c2VybmFtZSI6ImNsaXZleSIsImV4cCI6MTcxOTAxNDMxNX0.-qkVpCAZvwng-Suf55EPLAd4r-PHgVqqYFywjDtjnrUNL8hsdYyFMgFFPdE1wOhYYjI9izftfyY43pUayEQ57g'}
+        r = c.get('http://localhost/auctionhouse/auction/notvaliduuid', headers=header)
+        logger.info("STAT CODE IS [$d]", r.status_code)
+        assert r.status_code == 999
         assert r.headers.get('Content-Type') == 'application/json'
 
     def test_status_ok_no_auth(self):

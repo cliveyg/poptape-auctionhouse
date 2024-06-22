c2d111115b6dbb01fdd1ae49f7a2133a728cafde
cliveyg <junkcat@gmail.com>
2024-06-22 09:48:45 -0300

    Debug uuid test

--- auctionhouse/tests/test_api.py
+++ auctionhouse/tests/test_api.py
@@ -51,7 +51,6 @@ class TestAPIPaths(TransactionTestCase):
         c = RequestsClient()
         header = {'x-access-token': 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNfaWQiOiJmMzhiYTM5YS0zNjgyLTQ4MDMtYTQ5OC02NTlmMGJmMDUzMDQiLCJ1c2VybmFtZSI6ImNsaXZleSIsImV4cCI6MTcxOTAxNDMxNX0.-qkVpCAZvwng-Suf55EPLAd4r-PHgVqqYFywjDtjnrUNL8hsdYyFMgFFPdE1wOhYYjI9izftfyY43pUayEQ57g'}
         r = c.get('http://localhost/auctionhouse/auction/notvaliduuid', headers=header)
-
         assert r.status_code == 404
         assert r.headers.get('Content-Type') == 'application/json'
 
@@ -59,9 +58,10 @@ class TestAPIPaths(TransactionTestCase):
     @mock.patch('auctionhouse.authentication.requests.get', side_effect=mocked_auth_success)
     def test_get_auction_by_id(self, mock_get):
         c = RequestsClient()
-        logger.info("CRFETED AUC ID [%s]", self.auction_id)
         header = {'x-access-token': 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNfaWQiOiJmMzhiYTM5YS0zNjgyLTQ4MDMtYTQ5OC02NTlmMGJmMDUzMDQiLCJ1c2VybmFtZSI6ImNsaXZleSIsImV4cCI6MTcxOTAxNDMxNX0.-qkVpCAZvwng-Suf55EPLAd4r-PHgVqqYFywjDtjnrUNL8hsdYyFMgFFPdE1wOhYYjI9izftfyY43pUayEQ57g'}
         r = c.get('http://localhost/auctionhouse/auction/'+self.auction_id, headers=header)
+        returned_data = r.json()
+        logger.info("XOX Returned data is [%s]", returned_data)
         assert r.status_code == 200
         assert r.headers.get('Content-Type') == 'application/json'
 

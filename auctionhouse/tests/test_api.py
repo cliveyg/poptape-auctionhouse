7d836be5c36c00e45e8234189475f73aea49b835
cliveyg <junkcat@gmail.com>
2024-06-25 14:01:13 -0300

    Fixing combo create

--- auctionhouse/tests/test_api.py
+++ auctionhouse/tests/test_api.py
@@ -56,6 +56,28 @@ class TestAPIPaths(TransactionTestCase):
                                  algorithm='HS512')
 
     @mock.patch('auctionhouse.authentication.requests.get', side_effect=mocked_auth_success)
+    def test_combo_create_solo_auction_fail4(self, mock_get):
+        c = RequestsClient()
+        headers = {'x-access-token': self.token,
+                   'Content-Type': 'application/json'}
+        input_data = {
+            "type": "qq",
+            "start_time": "2024-06-23 20:18:21.910326",
+            "end_time": "2024-06-24 20:18:21.910326",
+            "quantity": 1,
+            "currency": "GBP",
+            "pay_visa": True,
+            "pay_cash": True,
+            "collection": True,
+            "name": "delete me"
+        }
+        r = c.post('http://localhost/auctionhouse/solo/auction/', data=json.dumps(input_data), headers=headers)
+        return_message = r.json()
+        logger.info("RET MESS IS %s", return_message)
+        assert return_message['message'] == "Unrecognized auction type"
+        assert r.status_code == 400
+
+    @mock.patch('auctionhouse.authentication.requests.get', side_effect=mocked_auth_success)
     def test_combo_create_solo_auction_fail3(self, mock_get):
         c = RequestsClient()
         headers = {'x-access-token': self.token,
@@ -71,7 +93,6 @@ class TestAPIPaths(TransactionTestCase):
         }
         r = c.post('http://localhost/auctionhouse/solo/auction/', data=json.dumps(input_data), headers=headers)
         return_message = r.json()
-        logger.info("RET MESS IS %s", return_message)
         assert return_message['message'] == "missing delivery options"
         assert r.status_code == 400
 

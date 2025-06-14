70c067744744924327a090d26319d200f7a148a9
cliveyg <junkcat@gmail.com>
2025-06-14 20:45:54 -0300

    Change Dockerfile to use python:3.12-slim - saves 1Gb in image size

--- auctionhouse/urls.py
+++ auctionhouse/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-# from django.conf.urls import url, include
 from django.conf.urls import include
-# from django.views.generic import RedirectView
 from auctionhouse.views import StatusView
 from auction.views import custom404
 

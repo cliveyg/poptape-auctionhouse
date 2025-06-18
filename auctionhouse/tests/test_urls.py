36c0d8ccbe7e7f1bb56db4fbcc331bb71971f8c2
cliveyg <junkcat@gmail.com>
2025-06-18 20:04:33 -0300

    Debug github action unit tests

--- auctionhouse/tests/test_urls.py
+++ auctionhouse/tests/test_urls.py
@@ -7,7 +7,7 @@ class TestURLS(SimpleTestCase):
 
     def test_api_status_route_is_resolved(self):
         url = reverse('status')
-        self.assertEquals(resolve(url).url_name, 'status')
-        self.assertEquals(resolve(url).route, 'auctionhouse/status/')
-        self.assertEquals(resolve(url).func.__name__, StatusView.as_view().__name__)
-        self.assertEquals(resolve(url).func.view_class,StatusView)
+        self.assertEqual(resolve(url).url_name, 'status')
+        self.assertEqual(resolve(url).route, 'auctionhouse/status/')
+        self.assertEqual(resolve(url).func.__name__, StatusView.as_view().__name__)
+        self.assertEqual(resolve(url).func.view_class,StatusView)
